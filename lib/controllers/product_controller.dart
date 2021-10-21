b675fa2478e1823709d219d8d7f669b59ec7339a
kamal614 <kamalkishorkumar614@gmail.com>
2021-10-21 15:45:49 +0530

    final commit

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,13 +1,17 @@
 import 'dart:ffi';
+import 'dart:isolate';
 
 import 'package:flutter/material.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:shopping/models/product.dart';
 import 'package:shopping/services/remote_services.dart';
 
 class ProductController extends GetxController {
   // var productList = <Welcome>[].obs;
+  var isLoading = true.obs;
   var productList = <Welcome>[];
+  //Welcome will be fetched from the product.dart usign Welcome class
   //late Welcome welcome;s
   @override
   void onInit() {
@@ -15,9 +19,18 @@ class ProductController extends GetxController {
     super.onInit();
   }
 
+//mow we'll call api from the controller
   void fetchProducts() async {
-    var products = await RemoteServices.fetchProducts();
-    // productList.value = products;
-    productList.addAll(products);
+    isLoading(true);
+    try {
+      var products = await RemoteServices.fetchProducts();
+      // productList.value = products;
+      // productList.addAll(products);
+      if (products != null) {
+        productList.addAll(products);
+      }
+    } finally {
+      isLoading(false);
+    }
   }
 }
--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -4,12 +4,17 @@ import 'package:shopping/models/product.dart';
 
 class RemoteServices {
   static var client = http.Client();
+  //we'll fetch the product and return to the controller
   static Future<List<Welcome>> fetchProducts() async {
+    //now we'll get the actual list of products
     var response = await client.get(Uri.parse(
         'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
+    //all the json data is now in the jsonString variable
     var jsonString = response.body;
+
     print(jsonString);
     return welcomeFromJson(jsonString);
+    //now the JSON data has been converted into the list using the function named welcomeFromJson which was in the product.dart
 
     // if (response.statusCode == 200) {
     //   var jsonString = response.body;
